356cb3efc56d5d5f865e4f269a23f5127f75b256
brian <riceb53@gmail.com>
2024-05-22 09:22:26 -0700

    make methods for one to many association, not using shortcut

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  # product only has 1 supplier
+  def supplier
+    # find 'my' supplier_id and find that value in the suppliers table
+    Supplier.find_by(id: supplier_id)
+  end
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,8 @@
 class Supplier < ApplicationRecord
+  # find all the products for this supplier
+  # find 'my' id, go to products table
+  # find allll the products that have my id
+  def products
+    Product.where(supplier_id: id)
+  end
 end
