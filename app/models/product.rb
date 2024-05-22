ec23744e87163c4bcc4518e4444984ffe9492061
brian <riceb53@gmail.com>
2024-05-22 09:38:03 -0700

    write has_many belongs_to methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
   # product only has 1 supplier
-  def supplier
-    # find 'my' supplier_id and find that value in the suppliers table
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   # find 'my' supplier_id and find that value in the suppliers table
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,7 +2,9 @@ class Supplier < ApplicationRecord
   # find all the products for this supplier
   # find 'my' id, go to products table
   # find allll the products that have my id
-  def products
-    Product.where(supplier_id: id)
-  end
+  has_many :products
+  
+  # def products
+  #   Product.where(supplier_id: id)
+  # end
 end
