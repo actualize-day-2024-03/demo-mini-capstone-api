52d8c4b32ea61bb7fd695466649f06483e7013ad
brian <riceb53@gmail.com>
2024-05-22 11:58:34 -0700

    add images to jbuilder

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   # product only has 1 supplier
   belongs_to :supplier
+  has_many :images
   # def supplier
   #   # find 'my' supplier_id and find that value in the suppliers table
   #   Supplier.find_by(id: supplier_id)
   # end
-  
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -6,3 +6,4 @@ json.description product.description
 json.created_at product.created_at
 json.updated_at product.updated_at
 json.supplier product.supplier
+json.images product.images
