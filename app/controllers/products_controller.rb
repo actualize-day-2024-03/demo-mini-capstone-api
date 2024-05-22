b19b9334a17ffd354d258a794ab7281ffe12889c
brian <riceb53@gmail.com>
2024-05-22 13:02:29 -0700

    fix tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,8 +23,7 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     @product.update(
       name: params[:name] || @product.name,
-      price: params[:price] || @product.price,
-      image_url: params[:image_url] || @product.image_url,
+      price: params[:price] || @product.price,      
       description: params[:description] || @product.description,
     )
     render :show
