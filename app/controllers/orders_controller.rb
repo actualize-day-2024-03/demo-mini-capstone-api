7a098fcefd7661a0e984796921f648694518bc6e
brian <riceb53@gmail.com>
2024-06-20 10:22:03 -0700

    orders create do calculations before backfilling carted_products data

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,37 +9,43 @@ class OrdersController < ApplicationController
   end
   
   def create
-    # user enters product id
-    # user enters quantity
-    # we figure everything else out
-    product = Product.find_by(id: params[:product_id])
-    tax_rate = 0.09
-    calculated_subtotal = params[:quantity].to_i * product.price
-    calculated_tax = (params[:quantity].to_i * product.price) * tax_rate
-    calculated_total = (params[:quantity].to_i * product.price) + ((params[:quantity].to_i * product.price) * tax_rate)
+    # items in current user's shopping cart
+    calculated_subtotal = 0
+    carted_products = CartedProduct.where(user_id: current_user.id, status: 'carted')
+    # loop through each item. find price, find quantity, multiply them, add to sum
+    # 170 => 170 * 1
+    # 540 => 270 * 2
+    carted_products.each do |cp|
+      calculated_subtotal += cp.quantity * cp.product.price
+    end
+
+    p calculated_subtotal
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    p calculated_tax
+    p calculated_total
+    
     
     @order = Order.new(
-      product_id: params[:product_id],
-      user_id: current_user.id,
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
-      )
-      @order.save
+      user_id: current_user.id, 
+      subtotal: calculated_subtotal, 
+      tax: calculated_tax, 
+      total: calculated_total
+    )
+    @order.save
+    render :show
+  end
+
+  def show
+    # alternate syntax instead of if statement
+    # @order = Order.find_by(id: params[:id], user_id: current_user.id)
+
+    @order = Order.find_by(id: params[:id])
+    if current_user.id == @order.user_id
       render :show
+    else
+      render json: {}
     end
+  end
 
-    def show
-      # alternate syntax instead of if statement
-      # @order = Order.find_by(id: params[:id], user_id: current_user.id)
-
-      @order = Order.find_by(id: params[:id])
-      if current_user.id == @order.user_id
-        render :show
-      else
-        render json: {}
-      end
-    end
-  
 end
