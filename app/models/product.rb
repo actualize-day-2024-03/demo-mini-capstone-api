85c3548b7f257fd54ddb32acb0d94a662aabd2c2
brian <riceb53@gmail.com>
2024-06-24 11:47:13 -0700

    default images from api

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,17 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+
+  def images_with_default
+    # show all the images unless it doesn't have any
+    if images.length > 0
+      return images
+      else
+        # if it doesn't ahve any, set a default image
+        return [{url: 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}]
+     end
+  end
+  
   # def supplier
   #   # find 'my' supplier_id and find that value in the suppliers table
   #   Supplier.find_by(id: supplier_id)
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -5,7 +5,7 @@ json.description product.description
 json.created_at product.created_at
 json.updated_at product.updated_at
 json.supplier product.supplier
-json.images product.images
+json.images product.images_with_default
 json.categories product.categories
 
 
