03e3cd5ac1c5db96b159c44423220f9fbc3a59a4
brian <riceb53@gmail.com>
2024-06-28 09:17:59 -0700

    remove authorization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   # before_action :authenticate_admin, except: [:index, :show]
-  before_action :authenticate_admin, only: [:create, :update, :destroy]
+  # before_action :authenticate_admin, only: [:create, :update, :destroy]
   
   def index
     if params[:name]
